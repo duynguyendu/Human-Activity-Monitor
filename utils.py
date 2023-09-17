a9da8101ef264cd13be08d84480b8fa7a7442ea0
ArtA <pthung7102002@gmail.com>
2023-09-17 19:08:44 +0700

    .

---
+++ utils.py
@@ -0,0 +1,34 @@
+from typing import List, Tuple
+
+import numpy as np
+import torch
+import cv2
+
+
+
+def extract_frames(path: str) -> np.ndarray[np.ndarray]:
+    """Create list of frames from video"""
+
+    cap = cv2.VideoCapture(str(path))
+    assert cap.isOpened(), "Error: Could not open video file."
+
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    cap.release()
+    return np.array(frames)
+
+
+def patching(image: np.ndarray, patch_size: Tuple[int, int]) -> np.ndarray[np.ndarray]:
+    """Divide image into smaller, non-overlapping regions or patches"""
+
+    patches = []
+    for y in range(0, image.shape[0], patch_size[1]):
+        for x in range(0, image.shape[1], patch_size[0]):
+            patch = image[y:y + patch_size[1], x:x + patch_size[0]]
+            patches.append(patch)
+    return np.array(patches)
+
