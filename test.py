f1fd82a51412fb958d787c67ce46c237084dff23
ArtA <pthung7102002@gmail.com>
2023-10-01 16:48:54 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from collections import Counter
 import random
 import os
 
-from modules.data import VideoProcessing
+from modules.transform import DataAugmentation
+from modules.processing import VideoProcessing
 from modules.model import LitModel
-from models import ViT_B_16
+from models import ViT_B_16, VGG11
 
 from lightning.pytorch import seed_everything
-from torchvision.datasets import ImageFolder
-import torchvision.transforms as T
 import torch
 
 import cv2
 import numpy as np
+from PIL import Image
 from rich import traceback, print
 traceback.install()
 
@@ -24,23 +24,25 @@ traceback.install()
 # Set number of worker (CPU will be used | Default: 80%)
 NUM_WOKER = int(os.cpu_count()*0.8) if torch.cuda.is_available() else 0
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-transform = T.Compose([
-    T.ToPILImage(),
-    T.Resize((224, 224), antialias=True),
-    T.ToTensor(),
-    T.Normalize(
-        mean=[0.485, 0.456, 0.406], 
-        std=[0.229, 0.224, 0.225]
-    )
-])
+TRANSFORM = DataAugmentation().DEFAULT
+
+VP = VideoProcessing(
+    sampling_value = 4, 
+    num_frames = 0, 
+    size = (750, 750)
+)
+
+DATA_PATH = "data/UTD-MHAD"
+
+MODEL = VGG11(num_classes=27, hidden_features=512)
+
+CHECKPOINT = "lightning_logs/VGG11_512_UTD/checkpoints/last.ckpt"
 
 
 
 def main(args):
-    DATA_PATH = "data/UCF11"
-
+    # Define dataset
     extensions = ['.mp4', '.avi', '.mkv', '.mov', '.flv', '.mpg']
-
     video_paths = []
 
     for root, dirs, files in os.walk(DATA_PATH):
@@ -48,44 +50,43 @@ def main(args):
             if any(file.lower().endswith(ext) for ext in extensions):
                 video_path = os.path.join(root, file)
                 video_paths.append(video_path)
-
-    classes = sorted(os.listdir("data/UCF11"))
+    
+    # Define classes
+    classes = sorted(os.listdir("data/UTD-MHAD_x/test"))
 
     # Define model
-    model = ViT_B_16(num_classes=11)
-    lit_model = LitModel(
-        model = model,
-        checkpoint = "lightning_logs/version_3/checkpoints/epoch=9-step=8370.ckpt"
-    ).to(DEVICE)
+    lit_model = LitModel(MODEL, checkpoint=CHECKPOINT).to(DEVICE)
 
-    VP = VideoProcessing(1, 0, (750, 750))
-
-    for path in random.sample(video_paths, 3):
+    # Iterate random video
+    for i, path in enumerate(random.sample(video_paths, 1)):
         results = []
         for frame in VP(path):
             with torch.inference_mode():
-                X = transform(frame).unsqueeze(0).to(DEVICE)
+                X = TRANSFORM(Image.fromarray(frame)).unsqueeze(0).to(DEVICE)
                 outputs = lit_model(X)
                 _, pred = torch.max(outputs, 1)
 
-            results.append(pred.item())
-            if len(results) > 4:
-                results = sorted(results, key=lambda x: Counter(results)[x])
-                results = results[1:-1]
+            results.append(classes[pred.item()])
 
-            unique_items, counts = np.unique(results, return_counts=True)
-            most_frequent_index = np.argmax(counts)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            cv2.putText(
-                frame, 
-                classes[unique_items[most_frequent_index]], 
-                (10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX, 
-                2, (255, 0, 0), 5
-            )
+            # cv2.putText(
+            #     frame, 
+            #     classes[unique_items[most_frequent_index]], 
+            #     (10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX, 
+            #     2, (255, 0, 0), 5
+            # )
 
             cv2.imshow("a", frame)
-            if cv2.waitKey(1) == ord('q'):
-                break
+
+            if cv2.waitKey(100) == ord('q'):
+                exit()
+
+        element_counts = Counter(results)
+
+        sorted_list = sorted(results, key=lambda x: (-element_counts[x], x), reverse=True)
+
+        print(Counter(sorted_list))
 
 
 
