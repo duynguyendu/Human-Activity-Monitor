d2d75b3a04e65a8cc8714dfdf10529ed3586b23d
ArtA <pthung7102002@gmail.com>
2023-10-05 10:53:49 +0700

    add lr moniter

--- modules/callback.py
+++ modules/callback.py
@@ -1,5 +1,6 @@
 from lightning.pytorch import Trainer, LightningModule
 import lightning.pytorch.callbacks as cb
+
 from rich import print
 
 
@@ -7,28 +8,35 @@ from rich import print
 class PrintTrainResult(cb.Callback):
     def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         epoch = trainer.current_epoch
-        results = trainer.callback_metrics
 
+        current_lr = ", ".join([f"{optim.param_groups[0]['lr']:.2e}" for optim in trainer.optimizers])
+
+        results = trainer.callback_metrics
         result_train = f"loss: {results['train/loss']:.4f}, acc: {results['train/accuracy']:.3f}"
         result_val = f"loss: {results['val/loss']:.4f}, acc: {results['val/accuracy']:.3f}"
 
-        print(f"[bold]Epoch[/]( {epoch} )  [bold]Train[/]({result_train})  [bold]Val[/]({result_val})")
+        print(
+            f"[bold]Epoch[/]( {epoch} ) "
+            f"[bold]Lr[/]( {current_lr} ) "
+            f"[bold]Train[/]({result_train}) "
+            f"[bold]Val[/]({result_val})"
+        )
 
 
-callbacks_list = [
+CustomCallbacks = [
     PrintTrainResult(),
     cb.RichModelSummary(),
     cb.RichProgressBar(),
     cb.LearningRateMonitor(logging_interval='epoch'),
     cb.ModelCheckpoint(
-        monitor='val/loss',
-        save_weights_only=True,
-        save_top_k=2,
-        save_last=True
+        monitor = 'val/loss',
+        save_weights_only = True,
+        save_top_k = 2,
+        save_last = True
     ),
     cb.EarlyStopping(
-        monitor='val/loss',
-        min_delta=0.0001,
-        patience=5
+        monitor = 'val/loss',
+        min_delta = 0.0001,
+        patience = 3
     )
 ]
