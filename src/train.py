fc228cf946f057b1918e44134f2b5dd5465cc910
ArtA <pthung7102002@gmail.com>
2023-10-26 19:13:01 +0700

    move main file to src

--- src/train.py
+++ src/train.py
@@ -1,13 +1,4 @@
-from omegaconf import DictConfig, open_dict
-import hydra
 import shutil
-
-from modules.scheduler import scheduler_with_warmup
-from modules.callback import custom_callbacks
-from modules.model import LitModel
-from modules.data import *
-from modules.utils import *
-from models import *
 
 import torch.optim.lr_scheduler as ls
 import torch.optim as optim
@@ -16,12 +7,26 @@ import torch
 
 from pytorch_lightning.loggers import TensorBoardLogger
 from lightning.pytorch import seed_everything, Trainer
+
+import hydra
+from omegaconf import DictConfig, open_dict
 from rich import traceback
 traceback.install()
 
+# Setup root directory
+import rootutils
+root = rootutils.setup_root(__file__, dotenv=True, pythonpath=True, cwd=False)
+
+from modules.scheduler import scheduler_with_warmup
+from modules.callback import custom_callbacks
+from modules.model import LitModel
+from modules.data import *
+from modules.utils import *
+from models import *
 
 
-@hydra.main(config_path="configs", config_name="train", version_base="1.3")
+
+@hydra.main(config_path="../configs", config_name="train", version_base="1.3")
 def main(cfg: DictConfig) -> None:
     # Remove the hydra outputs since we already have lightning logs
     shutil.rmtree("outputs")
