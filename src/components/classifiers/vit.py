2c7dacfa9237b93d1c4a93aac9cf383aea05c97a
ArtA <pthung7102002@gmail.com>
2023-10-28 01:34:35 +0700

    add vit to classifier component

---
+++ src/components/classifiers/vit.py
@@ -0,0 +1,36 @@
+import torch
+import os
+
+from modules.model import LitModel
+from models import ViT
+
+
+
+class ViT:
+    CLASESS = ['idle', 'laptop', 'phone', 'unknow']
+
+    def __init__(self, checkpoint: str, device: str='auto'):
+        if not os.path.exists(checkpoint):
+            raise FileNotFoundError(checkpoint)
+        self.ckpt = checkpoint
+
+        if device == 'auto':
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = device
+
+        self.model = LitModel(
+            model = ViT("B_32", num_classes=len(self.CLASESS)),
+            checkpoint = self.ckpt
+        ).to(self.device)
+
+
+    def __call__(self, X: torch.Tensor) -> str:
+        return self.predict(X)
+
+
+    def predict(self, X: torch.Tensor) -> str:
+        with torch.inference_mode():
+            outputs = self.model(X)
+            _, pred = torch.max(outputs, 1)
+
+        return self.CLASESS[pred.item()]
