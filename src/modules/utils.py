2b331b97e69ec6ffa927a76f10d9885f5736fc46
ArtA <pthung7102002@gmail.com>
2023-10-31 03:24:40 +0700

    add tuple_handler

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,13 +1,15 @@
+from typing import Union, Tuple, List
 import os
 
 
 __all__ = [
-    "workers_handler"
+    "workers_handler",
+    "tuple_handler"
 ]
 
 
 
-def workers_handler(value: int | float) -> int:
+def workers_handler(value: Union[int, float]) -> int:
     """
     Calculate the number of workers based on an input value.
 
@@ -28,3 +30,38 @@ def workers_handler(value: int | float) -> int:
     if not (-1 < workers < max_workers):
         raise ValueError(f"Number of workers is out of bounds. Min: 0 | Max: {max_workers}")
     return workers
+
+
+def tuple_handler(value: Union[int, List[int], Tuple[int]], max_dim: int) -> Tuple:
+    """
+    Create a tuple with specified dimensions and values.
+
+    Args:
+        value (Union[int, List[int], Tuple[int]]): The value(s) to populate the tuple with.
+            - If an integer is provided, a tuple with 'max_dim' elements, each set to this integer, is created.
+            - If a tuple or list of integers is provided, it should have 'max_dim' elements.
+        max_dim (int): The desired dimension (length) of the resulting tuple.
+
+    Returns:
+        Tuple: A tuple containing the specified values.
+
+    Raises:
+        TypeError: If 'max_dim' is not an integer or is less than or equal to 1.
+        TypeError: If 'value' is not an integer, tuple, or list.
+        ValueError: If the length of 'value' is not equal to 'max_dim'.
+    """
+
+    # Check max_dim
+    if not isinstance(max_dim, int) and max_dim > 1:
+        raise TypeError(f"The 'max_dim' parameter must be an int. Got {type(max_dim)} instead.")
+    # Check value
+    if isinstance(value, int):
+        output = tuple([value] * max_dim)
+    else:
+        try:
+            output = tuple(value)
+        except:
+            raise TypeError(f"The 'value' parameter must be an int or tuple or list. Got {type(value)} instead.")
+    if len(output) != max_dim:
+        raise ValueError(f"The lenght of 'value' parameter must be equal to {max_dim}. Got {len(output)} instead.")
+    return output
