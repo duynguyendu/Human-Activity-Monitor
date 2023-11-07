6521eef71d9df4784ea5cbc5608902f1c01ae5dd
ArtA <pthung7102002@gmail.com>
2023-11-08 05:12:16 +0700

    Create human_count.py

---
+++ src/components/features/human_count.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class HumanCount:
+    def __init__(self, smoothness: int = 1) -> None:
+        """
+        Initializes a HumanCount object.
+
+        Parameters:
+            smoothness (int): The number of recent values to consider for smoothing.
+                A higher value results in a smoother, but potentially slower, response.
+
+        Attributes:
+            smoothness (int): The smoothing parameter.
+            history (list): A list to store historical values for smoothing.
+        """
+        self.smoothness = smoothness
+        self.history = list()
+
+    def update(self, value: int) -> None:
+        """
+        Updates the history with a new value and maintains the specified smoothness.
+
+        Parameters:
+            value (int): The new value to add to the history.
+
+        Returns:
+            None
+        """
+        self.history.append(value)
+        if len(self.history) > self.smoothness:
+            self.history.pop(0)
+
+    def get_value(self) -> int:
+        """
+        Computes and returns the smoothed average of the historical values.
+
+        Returns:
+            int: The smoothed average of the historical values.
+        """
+        return int(np.mean(self.history))
