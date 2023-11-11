da7951c762bb18d4c154decac20892c60b443444
ArtA <pthung7102002@gmail.com>
2023-11-12 01:50:33 +0700

    add new config + track box

--- src/run.py
+++ src/run.py
@@ -19,28 +19,40 @@ from components.features import *
 from modules.video import Video
 
 
-@hydra.main(config_path="../configs", config_name="run", version_base="1.3")
+@hydra.main(config_path="../configs/run", config_name="run", version_base="1.3")
 def main(cfg: DictConfig) -> None:
     # Remove the hydra outputs since we already have lightning logs
     shutil.rmtree("outputs")
 
     # Define main componets
     # Detector
-    detector = YoloV8(**cfg["detector"], device=cfg["device"])
+    DETECTOR = YoloV8(
+        **cfg["detector"]["model"],
+        device=cfg["device"],
+    )
 
-    # Transform
-    transfrom = DataTransformation.TOPIL(image_size=cfg["classifier"]["image_size"])
+    if cfg["classifier"]:
+        # Transform
+        TRANSFORM = DataTransformation.TOPIL(image_size=cfg["classifier"]["image_size"])
 
-    # Classifier
-    classifier = ViT(checkpoint=cfg["classifier"]["checkpoint"], device=cfg["device"])
+        # Classifier
+        CLASSIFIER = ViT(
+            checkpoint=cfg["classifier"]["checkpoint"], device=cfg["device"]
+        )
 
-    # Open video
-    video = Video(path=cfg["video"]["path"])
+    # Load video
+    VIDEO = Video(path=cfg["video"]["path"])
+
+    # Track box
+    # Boxes initialize
+    [TrackBox.new(**box) for box in cfg["features"]["track_box"]["boxes"]]
+    # Configure boxes
+    TrackBox.config_boxes(**cfg["features"]["track_box"]["config"])
 
     # Config video progress bar
     progress_bar = tqdm(
-        total=video.total_frame,
-        desc=f"  {video.name}",
+        total=VIDEO.total_frame,
+        desc=f"  {VIDEO.name}",
         unit=" frame",
         miniters=1,
         smoothing=0.1,
@@ -48,66 +60,57 @@ def main(cfg: DictConfig) -> None:
     )
 
     # Frame loop
-    for frame in video.get_frame():
+    for frame in VIDEO.get_frame():
         # Frame sampling
-        if progress_bar.n % max(1, cfg["video"]["sampling"]) != 0:
+        if (progress_bar.n % max(1, cfg["video"]["speed"])) != 0:
             progress_bar.update(1)
             continue
 
         # First run setup
         if progress_bar.n == 0:
             print("[bold]Initialize:[/]")
-            add_text = lambda text, pos: cv2.putText(
-                frame,
-                text,
-                pos,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-            )
 
             # Initialize video writer
-            writer_cfg = cfg["video"]["save_output"]
-            if writer_cfg["enable"]:
+            writer_cfg = cfg["video"]["save"]
+            if writer_cfg:
                 save_path = os.path.join(
                     writer_cfg["save_path"],
-                    video.stem,
+                    VIDEO.stem,
                     writer_cfg["save_name"] + ".mp4",
                 )
-                writer = video.writer(save_path=save_path)
+                writer = VIDEO.writer(save_path=save_path)
                 print(f"  [bold]Saving output to:[/] [green]{save_path}[/]")
 
             # Initialize person count
-            human_count_cfg = cfg["feature"]["person_count"]
-            if human_count_cfg["enable"]:
+            human_count_cfg = cfg["features"]["human_count"]
+            if human_count_cfg:
                 human_counter = HumanCount(smoothness=human_count_cfg["smoothness"])
                 human_counter.save_config(
                     save_path=os.path.join(
                         human_count_cfg["save"]["save_path"],
-                        video.stem,
+                        VIDEO.stem,
                         human_count_cfg["save"]["save_name"] + ".csv",
                     ),
                     interval=human_count_cfg["save"]["interval"],
-                    sampling=cfg["video"]["sampling"],
+                    speed=cfg["video"]["speed"],
                 )
 
             # Initialize heatmap
-            heatmap_cfg = cfg["feature"]["heatmap"]
-            if heatmap_cfg["enable"]:
-                heatmap = Heatmap(shape=video.size(reverse=True))
+            heatmap_cfg = cfg["features"]["heatmap"]
+            if heatmap_cfg:
+                heatmap = Heatmap(shape=VIDEO.size(reverse=True))
 
                 # Heatmap config save
                 hm_save_path = os.path.join(
                     heatmap_cfg["save"]["save_path"],
-                    video.stem,
+                    VIDEO.stem,
                     heatmap_cfg["save"]["save_name"],
                 )
                 if heatmap_cfg["save"]["video"]:
                     heatmap.save_video(
                         save_path=hm_save_path + ".mp4",
-                        fps=video.fps,
-                        size=video.size(),
+                        fps=VIDEO.fps,
+                        size=VIDEO.size(),
                     )
                     print(
                         f"  [bold]Saving heatmap video to:[/] [green]{hm_save_path}.mp4[/]"
@@ -115,7 +118,7 @@ def main(cfg: DictConfig) -> None:
                 if heatmap_cfg["save"]["image"]:
                     heatmap.save_image(
                         save_path=hm_save_path + ".jpg",
-                        size=video.size(reverse=True),
+                        size=VIDEO.size(reverse=True),
                     )
                     print(
                         f"  [bold]Saving heatmap image to:[/] [green]{hm_save_path}.jpg[/]"
@@ -123,12 +126,12 @@ def main(cfg: DictConfig) -> None:
             print(f"[bold]Video progress:[/]")
 
         # Detect human
-        outputs = detector(frame)
+        outputs = DETECTOR(frame)
 
         # Heatmap
-        if heatmap_cfg["enable"]:
+        if heatmap_cfg:
             for output in outputs:
-                x1, y1, x2, y2 = output
+                x1, y1, x2, y2 = output["box"]
 
                 heatmap.update(
                     area=(x1, y1, x2, y2),
@@ -146,44 +149,54 @@ def main(cfg: DictConfig) -> None:
             )
 
         # Human count
-        if human_count_cfg["enable"]:
+        if human_count_cfg:
             # Update new value
             human_counter.update(value=len(outputs))
             # Add to frame
-            add_text(f"Person: {human_counter.get_value()}", pos=(20, 40))
+            VIDEO.add_text(
+                frame, text=f"Person: {human_counter.get_value()}", pos=(20, 40)
+            )
 
         # Human loop
         for output in outputs:
-            x1, y1, x2, y2 = output
+            x1, y1, x2, y2 = output["box"]
 
             # Human box
-            if cfg["feature"]["human_box"]:
+            if cfg["detector"]["show"]["human_box"]:
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
 
             # Human dot
             center = ((x1 + x2) // 2, (y1 + y2) // 2)
-            if cfg["feature"]["human_dot"]:
+            if cfg["detector"]["show"]["human_dot"]:
                 cv2.circle(frame, center, 5, (225, 225, 225), -1)
 
+            TrackBox.check(pos=center)
+
             # Classify action
-            if cfg["classifier"]["enable"]:
-                X = transfrom(output["human"])
+            if cfg["classifier"]:
+                X = TRANSFORM(output["human"])
 
-                result = classifier(X)
+                result = CLASSIFIER(X)
 
-                add_text(result, pos=(x1, y1 - 5))
+                VIDEO.add_text(frame, text=result, pos=(x1, y1 - 5))
 
-        cv2.imshow(video.stem, frame)
+            # Tracker
+            if cfg["detector"]["model"]["track"]:
+                VIDEO.add_text(frame, text=output["id"], pos=(x1, y1 - 5))
 
-        if writer_cfg["enable"]:
+        TrackBox.show(frame)
+
+        cv2.imshow(VIDEO.stem, frame)
+
+        if writer_cfg:
             writer.write(frame)
 
         progress_bar.update(1)
 
-        if not video.delay(cfg["video"]["delay"]):
+        if not VIDEO.delay(cfg["video"]["delay"]):
             break
 
-    (x.release() for x in (video, writer, heatmap))
+    [x.release() for x in (VIDEO, writer, heatmap)]
     cv2.destroyAllWindows()
 
 
