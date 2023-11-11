429d342e585eacebebdbb23ec90f9ea1ee9c3cb3
ArtA <pthung7102002@gmail.com>
2023-11-12 01:51:12 +0700

    add track

--- src/components/detectors/yolo_v8.py
+++ src/components/detectors/yolo_v8.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from functools import partial
 
 from cv2.typing import MatLike
 import torch
@@ -12,6 +13,7 @@ class YoloV8:
         weight: str = None,
         conf: float = 0.25,
         iou: float = 0.3,
+        track: bool = False,
         device: str = "auto",
     ):
         """
@@ -26,6 +28,7 @@ class YoloV8:
         if device == "auto":
             device = "cuda" if torch.cuda.is_available() else "cpu"
         self.device = device
+        self.track = track
         self.model = YOLO(weight if weight else "weights/yolov8x.pt").to(self.device)
         self.config = {"conf": conf, "iou": iou}
 
@@ -51,13 +54,25 @@ class YoloV8:
         Returns:
             List: A list containing box of humans detected
         """
-        result = self.model.predict(image, **self.config, classes=0, verbose=False)[0]
+        model = self.model.track if self.track else self.model.predict
+
+        model = partial(model, source=image, classes=0, **self.config, verbose=False)
+
+        result = (
+            model(
+                persist=True,
+                tracker="configs/tracker.yaml",
+            )
+            if self.track
+            else model()
+        )[0]
 
         outputs = []
         if result.boxes:
             for box in result.boxes:
-                x1, y1, x2, y2 = (int(i.item()) for i in box.data[0][:4])
-
-                outputs.append((x1, y1, x2, y2))
+                x1, y1, x2, y2, idx = (int(i.item()) for i in box.data[0][:5])
+                outputs.append(
+                    {"box": (x1, y1, x2, y2), "id": idx if self.track else None}
+                )
 
         return outputs
