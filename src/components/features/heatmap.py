42a552a573b08f0c1ea9a55f8079b2aa88007e16
ArtA <pthung7102002@gmail.com>
2023-11-23 16:52:06 +0700

    update heatmap

--- src/components/features/heatmap.py
+++ src/components/features/heatmap.py
@@ -8,26 +8,39 @@ from src.modules.utils import tuple_handler
 
 
 class Heatmap:
-    def __init__(self, shape: Tuple[int, int]) -> None:
+    def __init__(
+        self,
+        shape: Tuple[int, int],
+        grow_value: int = 3,
+        decay_value: int = 1,
+        blurriness: float = 1.0,
+        alpha: float = 0.5,
+    ) -> None:
         """
         Initializes a Heatmap object.
 
         Args:
-            layer (np.ndarray): starting layer
+            shape (Tuple[int, int]): Initiate layer.
+            grow_value (int, optional): Value to grow. Defaults to 3
+            decay_value (int, optional): Value to decay. Defaults to 1
+            blurriness (float, optional): The blurriness of the heat layer. Defaults to 1.0
+            alpha (float, optional): Opacity of the heat layer. Defaults to 0.5
         """
         self.layer = np.zeros(shape=tuple_handler(shape, max_dim=2), dtype=np.uint8)
+        self.grow_value = grow_value
+        self.decay_value = decay_value
+        self.blurriness = blurriness
+        self.alpha = alpha
 
-    def update(self, area: Tuple, value: int) -> np.ndarray:
+    def update(self, area: Tuple) -> np.ndarray:
         """
         Update map layer
 
         Args:
-            area (int): area to increase
-            value (_type_): amount to update
-            blurriness (float, optional): the blurriness of the heat layer. Defaults to 1.0
+            area (int): Area to increase
 
         Returns:
-            np.ndarray: layer after updated
+            np.ndarray: Layer after updated
         """
 
         # Grow
@@ -35,38 +48,31 @@ class Heatmap:
         x1, y1 = int(x1 * 0.95), int(y1 * 0.95)
         x2, y2 = int(x2 * 1.05), int(y2 * 1.05)
         self.layer[y1:y2, x1:x2] = np.minimum(
-            self.layer[y1:y2, x1:x2] + value, 255 - value
+            self.layer[y1:y2, x1:x2] + self.grow_value, 255 - self.grow_value
         )
 
-    def decay(self, value: int = 1) -> None:
+    def decay(self) -> None:
         """
-        Reduce heatmap value
+        Reduce heatmap
 
         Args:
             value (int): percentage to decrease
         """
-        self.layer = ((1 - value / 100) * self.layer).astype(np.uint8)
+        self.layer = ((1 - self.decay_value / 100) * self.layer).astype(np.uint8)
 
-    def apply(
-        self,
-        image: np.ndarray,
-        blurriness: float = 1.0,
-        alpha: float = 0.5,
-    ) -> np.ndarray:
+    def apply(self, image: np.ndarray) -> np.ndarray:
         """
         Apply heatmap
 
         Args:
             image (np.ndarray): image to apply heatmap
-            layer (np.ndarray): layer to apply heat
-            alpha (float, optional): Opacity of the heat layer. Defaults to 0.5
 
         Returns:
             np.ndarray: result image
         """
 
         # Blur
-        blurriness = int(blurriness * 100)
+        blurriness = int(self.blurriness * 100)
         blurriness = blurriness + 1 if blurriness % 2 == 0 else blurriness
         self.layer = cv2.stackBlur(self.layer, (blurriness, blurriness), 0)
 
@@ -74,7 +80,7 @@ class Heatmap:
         heatmap = cv2.applyColorMap(self.layer, cv2.COLORMAP_TURBO)
 
         # Combined image and heat layer
-        cv2.addWeighted(heatmap, alpha, image, 1 - alpha, 0, image)
+        cv2.addWeighted(heatmap, self.alpha, image, 1 - self.alpha, 0, image)
 
         # Check if save video
         if hasattr(self, "_video_writer"):
@@ -102,8 +108,8 @@ class Heatmap:
         Args:
             save_path (str): path to store writed video.
             fps (int): FPS of output video.
-            size (Tuple): size of output video.
-            codec (str, optional): codec for write video. Defaults to "mp4v".
+            size (Tuple): Size of output video.
+            codec (str, optional): Codec for write video. Defaults to "mp4v".
 
         Returns:
             None
@@ -122,19 +128,21 @@ class Heatmap:
         )
 
     def save_image(self, save_path: str, size: Tuple) -> None:
-        """_summary_
+        """Save an image to the specified path with the given size.
 
         Args:
-            save_path (str): _description_
-            size (Tuple): _description_
+            save_path (str): The path where the image will be saved.
+            size (Tuple): A tuple representing the dimensions of the image (width, height).
+
+        Returns:
+            None
         """
-        save_path = Path(save_path)
 
         #  Create save folder
-        save_path.parent.mkdir(parents=True, exist_ok=True)
+        Path(save_path).parent.mkdir(parents=True, exist_ok=True)
 
         self._image_writer = {
-            "path": str(save_path),
+            "path": save_path,
             "count": 0,
             "image": np.zeros(tuple_handler((*size, 3), max_dim=3), dtype=np.float32),
         }
