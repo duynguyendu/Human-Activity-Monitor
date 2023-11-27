dc5adbd3af92d2bdd6e4a8a95ebbf1fd77c226d5
ArtA <pthung7102002@gmail.com>
2023-11-28 04:36:21 +0700

    change apply to return player

--- src/components/features/heatmap.py
+++ src/components/features/heatmap.py
@@ -14,7 +14,6 @@ class Heatmap:
         grow_value: int = 3,
         decay_value: int = 1,
         blurriness: float = 1.0,
-        alpha: float = 0.5,
     ) -> None:
         """
         Initializes a Heatmap object.
@@ -24,13 +23,11 @@ class Heatmap:
             grow_value (int, optional): Value to grow. Defaults to 3
             decay_value (int, optional): Value to decay. Defaults to 1
             blurriness (float, optional): The blurriness of the heat layer. Defaults to 1.0
-            alpha (float, optional): Opacity of the heat layer. Defaults to 0.5
         """
         self.layer = np.zeros(shape=tuple_handler(shape, max_dim=2), dtype=np.uint8)
         self.grow_value = grow_value
         self.decay_value = decay_value
         self.blurriness = blurriness
-        self.alpha = alpha
 
     def update(self, area: Tuple) -> np.ndarray:
         """
@@ -60,9 +57,9 @@ class Heatmap:
         """
         self.layer = ((1 - self.decay_value / 100) * self.layer).astype(np.uint8)
 
-    def apply(self, image: np.ndarray) -> np.ndarray:
+    def get(self) -> np.ndarray:
         """
-        Apply heatmap
+        Get the current heatmap
 
         Args:
             image (np.ndarray): image to apply heatmap
@@ -79,9 +76,6 @@ class Heatmap:
         # Apply heat to layer
         heatmap = cv2.applyColorMap(self.layer, cv2.COLORMAP_TURBO)
 
-        # Combined image and heat layer
-        cv2.addWeighted(heatmap, self.alpha, image, 1 - self.alpha, 0, image)
-
         # Check if save video
         if hasattr(self, "_video_writer"):
             self._video_writer.write(heatmap)
@@ -97,7 +91,7 @@ class Heatmap:
                 ),
             )
 
-        return image, heatmap
+        return heatmap
 
     def save_video(
         self, save_path: str, fps: int, size: Tuple, codec: str = "mp4v"
