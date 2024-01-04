3d624278912f063b57980fa6fc1de117193ada77
ArtA <pthung7102002@gmail.com>
2024-01-05 03:13:21 +0700

    remove track

--- src/components/detector.py
+++ src/components/detector.py
@@ -16,7 +16,6 @@ class Detector:
     def __init__(
         self,
         weight: str = "weights/yolov8x.pt",
-        track: bool = False,
         conf: float = 0.25,
         iou: float = 0.7,
         size: Union[int, Tuple] = 640,
@@ -26,14 +25,12 @@ class Detector:
         optimize: bool = False,
         backend: str = None,
         device: str = "auto",
-        **kwargs
     ):
         """
         Initialize the Yolo-v8 model
 
         Args:
             weight (str, optional): Path to the YOLO model weights file. Defaults to None.
-            track (bool, optional): Enable object tracking. Defaults to False.
             conf (float, optional): Confidence threshold for object detection. Defaults to 0.25.
             iou (float, optional): Intersection over Union (IoU) threshold. Defaults to 0.3.
             size (int or Tuple, optional): Input size for the YOLO model. Defaults to 640.
@@ -45,21 +42,20 @@ class Detector:
             device (str, optional): Device to run the model ('auto', 'cuda', or 'cpu'). Defaults to "auto".
         """
         self.device = device_handler(device)
-        self.model = self.__get_model(
+        self.config = {
+            "conf": conf,
+            "iou": iou,
+            "imgsz": size,
+            "half": self.__check_half(half),
+            "device": self.device,
+        }
+        self.model = self.__setup_model(
             weight=weight,
-            track=track,
             fuse=fuse,
             format="onnx" if onnx else "pt",
             optimize=optimize,
             backend=backend,
-            config={
-                "conf": conf,
-                "iou": iou,
-                "imgsz": size,
-                "half": self.__check_half(half),
-                "device": self.device,
-            },
-            **kwargs
+            config=self.config,
         )
 
     def __call__(self, image: Union[cv2.Mat, np.ndarray]) -> List[Tuple]:
@@ -111,23 +107,20 @@ class Detector:
             },
         )
 
-    def __get_model(
+    def __setup_model(
         self,
         weight: str,
-        track: bool,
         fuse: bool,
         format: str,
         optimize: bool,
         backend: str,
         config: Dict,
-        **kwargs
     ) -> partial:
         """
         Create and configure the YOLO model based on the provided parameters.
 
         Args:
             weight (str): Path to the YOLO model weights file.
-            track (bool): Enable object tracking.
             fuse (bool): Fuse model layers for improved performance.
             format (str): Model format.
             optimize (bool): Enable model optimization using torch.compile.
@@ -137,8 +130,9 @@ class Detector:
         Returns:
             partial: Partially configured YOLO model.
         """
+
         # Create an instance of the YOLO model
-        model = YOLO(weight, task="detect")
+        model = YOLO(model=weight, task="detect")
 
         # Fuse model layers if specified
         if fuse:
@@ -148,15 +142,8 @@ class Detector:
         if optimize:
             model = self.__compile(X=model, backend=backend)
 
-        # Configure the track model if specified
-        if track:
-            model = model.track
-            config.update({"persist": True, "tracker": "configs/tracker.yaml"})
-        else:
-            model = model.predict
-
         # Return a partially configured YOLO model
-        return partial(model, **config, classes=0, verbose=False, **kwargs)
+        return partial(model.predict, **config, classes=0, verbose=False)
 
     def __check_half(self, half: bool) -> bool:
         """
@@ -168,15 +155,17 @@ class Detector:
         Returns:
             bool: False if the device is on CPU; otherwise, keep the original value.
         """
+
         # Check if half precision is specified and the device is CPU
         if half and self.device == "cpu":
             print(
-                "[yellow][WARNING] [YOLOv8]: Half is only supported on CUDA. Using default float32.[/]"
+                "[yellow][WARNING] [Detector]: Half is only supported on CUDA. Using default float32.[/]"
             )
             half = False
+
         return half
 
-    def forward(self, image: Union[cv2.Mat, np.ndarray]) -> List[Tuple]:
+    def forward(self, image: Union[cv2.Mat, np.ndarray]) -> np.ndarray:
         """
         Perform a forward pass of the model.
 
@@ -184,22 +173,11 @@ class Detector:
             image (MatLike): Input image.
 
         Returns:
-            List: A list containing detected human bounding boxes.
+            np.ndarray: An array containing information of detected people.
         """
+
         # Perform a forward pass of the model on the input image
         result = self.model(source=image)[0]
 
-        outputs = []
-
-        # Extract information from the detection results
-        if result.boxes:
-            for box in result.boxes:
-                outputs.append(
-                    {
-                        "box": [int(i.item()) for i in box.xyxy[0]],
-                        "score": box.conf.item(),
-                        "id": int(box.id.item()) if box.is_track else None,
-                    }
-                )
-
-        return outputs
+        # Get result
+        return result.boxes.data.cpu().numpy()
