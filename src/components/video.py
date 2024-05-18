296a00adc09e037198a449829fe238fe1ffa1bea
duy <nguyenduduy233@gmail.com>
2024-05-18 18:07:02 +0700

    Remove ffmpeg

--- src/components/video.py
+++ src/components/video.py
@@ -57,7 +57,7 @@ class Video:
         #         "File not found. Check again or use an absolute path."
         #     )
         self.path = str(path)
-        self.video_capture = cv2.VideoCapture(path, cv2.CAP_FFMPEG)
+        self.video_capture = cv2.VideoCapture(path)
         self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
         self.is_camera = bool(self.total_frame == -1)
